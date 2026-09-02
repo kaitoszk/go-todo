58a3360f3b91077fbbc1a7029cd230a61c7ef89c
kaitoszk <84959703+kaitoszk@users.noreply.github.com>
2026-09-03 08:59:19 +0900

    repoのテスト実装完了

--- internal/repository/todo_repository_test.go
+++ internal/repository/todo_repository_test.go
@@ -352,7 +352,7 @@ func TestDelete(t *testing.T) {
 		},
 		{
 			name: "異常系_Execが失敗",
-			id: 1,
+			id:   1,
 			setupMock: func(mock sqlmock.Sqlmock) {
 				mock.ExpectExec(regexp.QuoteMeta(query)).
 					WithArgs(1).
@@ -384,5 +384,62 @@ func TestDelete(t *testing.T) {
 			}
 		})
 	}
-	
+}
+
+func TestToggle(t *testing.T) {
+	const query = "UPDATE todos SET done = NOT done WHERE id = $1"
+
+	tests := []struct {
+		name      string
+		id        int
+		setupMock func(mock sqlmock.Sqlmock)
+		wantErr   bool
+	}{
+		{
+			name: "正常系_1件更新",
+			id: 1,
+			setupMock: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(regexp.QuoteMeta(query)).
+					WithArgs(1).
+					WillReturnResult(sqlmock.NewResult(0, 1))
+			},
+			wantErr: false,
+		},
+		{
+			name: "異常系_Execが失敗",
+			id: 1,
+			setupMock: func(mock sqlmock.Sqlmock) {
+				mock.ExpectExec(regexp.QuoteMeta(query)).
+					WithArgs(1).
+					WillReturnError(errors.New("update失敗"))
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Errorf("sqlmockの生成に失敗: %v", err)
+			}
+			defer db.Close()
+
+			defer func() {
+				if err := mock.ExpectationsWereMet(); err != nil {
+					t.Errorf("消化されていない期待がある: %v", err)
+				}
+			}()
+
+
+			tt.setupMock(mock)
+
+			repo := NewTodoRepository(db)
+			err = repo.Toggle(tt.id)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("wantErr=%v, got err=%v", tt.wantErr, err)
+			}
+		})
+	}
 }
